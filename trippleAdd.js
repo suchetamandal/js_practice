ee4c1a1732e7a0a21036fc17e5cffc2be3aa9117
SUCHETA MANDAL <sucheta.gcetts@gmail.com>
2018-04-06 11:55:03 -0700

    Update trippleAdd.js

--- trippleAdd.js
+++ trippleAdd.js
@@ -8,6 +8,15 @@ tripple = (num1 => {
 
 tripple(20)(30)(40)
 
+function quadrupleAdd(num1) {
+  return function(num2) {
+    return function(num3, num4) {
+      return num1 + num2 + num3 + num4;
+    };
+  };
+}
+quadrupleAdd(10)(20)(30, 40);
+
 
 /* Currying Functions */
 when a function, instead of taking all arguments at one time,
