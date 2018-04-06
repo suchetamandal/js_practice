4a1ebf94de89473dbd26fdf8dc7bc8ee8596b337
SUCHETA MANDAL <sucheta.gcetts@gmail.com>
2018-04-06 12:09:05 -0700

    Create generateDynamicButtons.js

---
+++ generateDynamicButtons.js
@@ -0,0 +1,13 @@
+function createButtons() {
+   for (var i = 1; i <= 5; i++) {
+     var body = document.getElementsByTagName("BODY")[0];
+     var button = document.createElement("BUTTON");
+     button.innerHTML = 'Button ' + i;
+     button.onclick = function() {
+          alert('This is button ' + i);
+     }
+     body.appendChild(button);
+   }
+}
+ 
+createButtons();
