93c1740290c6f353152dadc9a0c0d31c9f3c82d7
SUCHETA MANDAL <sucheta.gcetts@gmail.com>
2018-04-06 12:17:00 -0700

    Update generateDynamicButtons.js

--- generateDynamicButtons.js
+++ generateDynamicButtons.js
@@ -1,13 +1,34 @@
 function createButtons() {
-   for (var i = 1; i <= 5; i++) {
+   for (let i = 1; i <= 5; i++) {
      var body = document.getElementsByTagName("BODY")[0];
      var button = document.createElement("BUTTON");
      button.innerHTML = 'Button ' + i;
      button.onclick = function() {
           alert('This is button ' + i);
-     }
+     };
      body.appendChild(button);
    }
 }
  
 createButtons();
+
+
+Another Solution using IIFE:-
+  
+function createButtons() {
+   for (let i = 1; i <= 5; i++) {
+     var body = document.getElementsByTagName("BODY")[0];
+     var button = document.createElement("BUTTON");
+     button.innerHTML = 'Button ' + i;
+     (function(number){
+         button.onclick = function() {
+          alert('This is button ' + i);
+     };
+     })(i);
+     body.appendChild(button);
+   }
+}
+ 
+createButtons();
+
+//So in this case iffe will store i value in num hence it will return correct result.
