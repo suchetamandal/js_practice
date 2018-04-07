5df20c50597136f84d14efc981f0761813ae2157
SUCHETA MANDAL <sucheta.gcetts@gmail.com>
2018-04-07 01:36:54 -0700

    Create listRelated.js

---
+++ listRelated.js
@@ -0,0 +1,17 @@
+const list1 = [1, 2, 3, 4, 5];
+const list2 = list1;
+ 
+list1.push(6, 7, 8);
+ 
+console.log(list2);// [1,2,3,4,5,6,7,8]
+
+
+
+const list1 = [1, 2, 3, 4, 5];
+const list2 = list1.slice();
+// const list2 = list1.concat([]);
+ 
+list1.push(6, 7, 8);
+ 
+console.log('List 1: ', list1); //[1,2,3,4,5,6,7,8]
+console.log('List 2: ', list2); // [1,2,3,4,5]
