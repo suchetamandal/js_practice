6ccec143c390faa425e769c97f1c23100550f4b0
SUCHETA MANDAL <sucheta.gcetts@gmail.com>
2018-04-07 11:48:08 -0700

    Create data_type_checking.js

---
+++ data_type_checking.js
@@ -0,0 +1,6 @@
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
+console.log(typeof {}); // object
+console.log(typeof []); // object
+console.log(Array.isArray([])); //true
+console.log([] instanceof Array); //true
